54185846b4b57493b65141cd3a728f2faa68fa6c
Sadly4343 <loganhartshorn@gmail.com>
2025-03-21 01:28:57 -0600

    updates

--- src/js/main.js
+++ src/js/main.js
@@ -13,5 +13,18 @@ closeButton.addEventListener("click", () => {
   modals.style.display = "none";
 });
 
+const modals1 = document.getElementById("modal1");
+const button1 = document.getElementById("callBtn1");
+const closeButton1 = document.querySelector("#closeBtn1");
+
+button1.addEventListener("click", () => {
+  modals1.style.display = "block";
+});
+
+closeButton1.addEventListener("click", () => {
+  modals1.style.display = "none";
+});
+
+
 loadHeaderFooter();
 
